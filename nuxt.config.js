1a7c8e1c2b04e006b8c6787779e59764fbe31800
dianw <dianw.aditya@gmail.com>
2017-09-03 17:59:11 +0700

    Add basic coreui

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,16 +7,33 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'TheSkeleton Admin UI' }
+      { hid: 'description', name: 'description', content: 'TheSkeleton Admin' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+
+  css: [
+    '~/assets/css/style.scss'
+  ],
+
   /*
   ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
+
+  modules: [
+    [ '@nuxtjs/axios', { baseURL: '/api' } ],
+    [ '@nuxtjs/bootstrap-vue', { css: false } ],
+    '@nuxtjs/font-awesome'
+  ],
+
+  router: {
+    base: '/admin',
+    linkActiveClass: 'active'
+  },
+
   /*
   ** Build configuration
   */
@@ -33,6 +50,14 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
-    }
+    },
+    extractCSS: true,
+    vendor: [
+      'axios',
+      'bootstrap',
+      '@nuxtjs/axios',
+      '@nuxtjs/bootstrap-vue',
+      '@nuxtjs/font-awesome'
+    ]
   }
 }
